6b82b9a6f14d783b954381874f3200efd4777447
Narayandwivedi <desinplus1@gmail.com>
2025-11-06 18:40:52 +0530

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const connectDB = require('./utils/mongodb')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 // CORS configuration
 const allowedOrigins = [
